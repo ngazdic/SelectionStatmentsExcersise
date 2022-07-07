e3f136a6e45ce55fad5d8bbfc32defafcbef9493
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-07-07 11:52:09 -0400

    if/else statment made

---
+++ SelectionStatmentsExcersise/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+var r = new Random();
+var favNumber = r.Next(1, 1000);
+
+Console.WriteLine("Try to guess my favourite number");
+
+var userInput = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+if (favNumber < userInput)
+{
+
+    Console.WriteLine("too high");
+} else if (favNumber > userInput)
+{
+
+    Console.WriteLine("too low");
+
+}
+
+else
+{
+
+    Console.WriteLine("You guessed it!!!");
+}
